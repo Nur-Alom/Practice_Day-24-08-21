4aa1ed598778a526daf38ef078ff087d890b330c
Nur-Alom <alamn7150@gmail.com>
2021-08-25 01:19:17 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,22 @@
+
+// const name = 'Nur-Alom';
+// let number = '01778799787';
+
+// const car = {
+//     name: 'Honda',
+//     modle: 'king White',
+//     price: 3075500,
+//     color: 'White',
+//     milage: '35km'
+// };
+
+
+// const value = `${name} ${number} 
+// ${car.price} ${car.milage} ${car.color} ${car.modle}
+// `;
+// console.log(value, car);
+
+
+
+
+
