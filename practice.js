540b152535c3faf7119093bebac29cad2b25b680
Nur-Alom <alamn7150@gmail.com>
2021-08-25 17:32:39 +0600

    commit

--- practice.js
+++ practice.js
@@ -2,6 +2,7 @@
 // const name = 'Nur-Alom';
 // let number = '01778799787';
 
+
 // const car = {
 //     name: 'Honda',
 //     modle: 'king White',
@@ -9,8 +10,6 @@
 //     color: 'White',
 //     milage: '35km'
 // };
-
-
 // const value = `${name} ${number} 
 // ${car.price} ${car.milage} ${car.color} ${car.modle}
 // `;
@@ -28,9 +27,9 @@
 
 
 // const multi = (num1, num2, num3) => num1 * num2 * num3;
-
 // const result2 = multi(5, 5, 6);
 // console.log(result2);
+
 
 // const multi = (num1, num2) => {
 //     const add1 = num1 + 2;
@@ -44,23 +43,38 @@
 
 // const array = [5, 7, 8, 9, 20, 10, 30, 32, 21];
 // const ans = array.map(array => array * 5);
-
 // console.log(ans);
 
 
 // const array = [5, 7, 8, 9, 20, 10, 33, 32, 21];
 // const ans = array.filter(array => array % 2);
-
 // console.log(ans);
 
-const mobile = [
-    { name: 'Mi', modle: 'Mi A2 lite', price: 17500, color: 'Blue' },
-    { name: 'samsung', modle: 'samsung galaxy a30', price: 20500, color: 'Red' },
-    { name: 'symphony', modle: 'symphony v75', price: 5000, color: 'Black' }
-];
 
-const find = mobile.find(mobile => mobile.price == 5000);
-console.log(find);
+
+// const mobile = [
+//     { name: 'Mi', modle: 'Mi A2 lite', price: 17500, color: 'Blue' },
+//     { name: 'samsung', modle: 'samsung galaxy a30', price: 20500, color: 'Red' },
+//     { name: 'symphony', modle: 'symphony v75', price: 5000, color: 'Black' }
+// ];
+// const find = mobile.find(mobile => mobile.price == 5000);
+// console.log(find);
+
+
+
+// 2. Arrow Function.
+
+// function number(num1, num2, num3 = 7) {
+//     const sum = num1 + num2 + num3;
+//     return sum;
+// };
+// const callNumber = number(5, 5);
+// console.log(callNumber);
+
+
+// const number = (num1, num2, num3 = 7) => num1 + num2 + num3;
+// const callNumber = number(5, 5);
+// console.log(callNumber);
 
 
 
