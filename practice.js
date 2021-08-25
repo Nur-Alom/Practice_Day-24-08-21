fa08cdbb8f30326c34b28e684c3824ff5fdf0235
Nur-Alom <alamn7150@gmail.com>
2021-08-25 12:09:37 +0600

    commit

--- practice.js
+++ practice.js
@@ -17,6 +17,52 @@
 // console.log(value, car);
 
 
+// const dev = (num1, num2) => {
+//     const add1 = num1 + 2;
+//     const add2 = num2 + 2;
+//     const multy = add1 * add2;
+//     return multy;
+// }
+// const result = dev(2, 2);
+// console.log(result);
+
+
+// const multi = (num1, num2, num3) => num1 * num2 * num3;
+
+// const result2 = multi(5, 5, 6);
+// console.log(result2);
+
+// const multi = (num1, num2) => {
+//     const add1 = num1 + 2;
+//     const add2 = num2 + 2;
+//     const multi = add1 * add2;
+//     return multi;
+// }
+// const result = multi(10, 10);
+// console.log(result);
+
+
+// const array = [5, 7, 8, 9, 20, 10, 30, 32, 21];
+// const ans = array.map(array => array * 5);
+
+// console.log(ans);
+
+
+// const array = [5, 7, 8, 9, 20, 10, 33, 32, 21];
+// const ans = array.filter(array => array % 2);
+
+// console.log(ans);
+
+const mobile = [
+    { name: 'Mi', modle: 'Mi A2 lite', price: 17500, color: 'Blue' },
+    { name: 'samsung', modle: 'samsung galaxy a30', price: 20500, color: 'Red' },
+    { name: 'symphony', modle: 'symphony v75', price: 5000, color: 'Black' }
+];
+
+const find = mobile.find(mobile => mobile.price == 5000);
+console.log(find);
+
+
 
 
 
